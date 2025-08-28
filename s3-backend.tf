04cf9e2a10af8a24da16ee1be8a85bc6d0f3b333
Andy Nzeka <andynze4@gmail.com>
2025-08-27 20:49:15 -0500

    Push

--- s3-backend.tf
+++ s3-backend.tf
@@ -1,9 +1,28 @@
-terraform {
-  backend "s3" {
-    bucket = "my-terraform-state85"
-    key    = "environments2"
-    region = "us-east-1"
-    #    dynamodb_table = "ndcc-terraform-state-lock-dynamo"
-    encrypt = true
+# terraform {
+#   backend "s3" {
+#     bucket = "my-terraform-state85"
+#     key    = "environments2"
+#     region = "us-east-1"
+#     #    dynamodb_table = "my-terraform-eks-state-lock"
+#     encrypt = true
+#   }
+# }
+
+resource "aws_s3_bucket" "eks-bucket" {
+  bucket = "dml-terraform-state"
+
+  lifecycle  {
+    prevent_destroy = false
   }
+}
+
+resource "aws_dynamodb_table" "my-dynamodb-table" {
+  name           = "dml-terraform-eks-state-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
 }
